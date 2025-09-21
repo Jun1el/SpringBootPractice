8d28efe01db8f1e598b4545d08058461a8303a25
Jun1el <latorrevasquezandressebastian@gmail.com>
2025-09-21 13:07:23 -0500

    Doc: documentacion para clase PalindromoRest

--- src/main/java/SpringPractice/controllers/PalindromoRest.java
+++ src/main/java/SpringPractice/controllers/PalindromoRest.java
@@ -4,8 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controlador para ver si una palabra es palindroma
+ */
 @RestController
 public class PalindromoRest {
+    /**
+     *Endpoint para vereficiar si es palindromo
+     * @param palabra que sera verificada
+     */
     @GetMapping("/palindrome/{palabra}")
     public String palindromo(@PathVariable String palabra) {
         if (isPalindromo(palabra)) {
@@ -14,6 +21,12 @@ public class PalindromoRest {
             return "La palabra "+palabra+" No es Palindromo";
         }
     }
+
+    /**
+     * Metodo para verificar a traves de un bucle que recorre la mitad de sus letras y comparandolas
+     * @param palabra
+     * @return
+     */
     public boolean isPalindromo(String palabra) {
         for (int i = 0; i < palabra.length() / 2; i++) {
             if (palabra.charAt(i) != palabra.charAt(palabra.length() - i - 1)) {
