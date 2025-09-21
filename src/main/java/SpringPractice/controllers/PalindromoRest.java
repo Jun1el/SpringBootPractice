82e233a82627c972726552ec1b15610c3945bf49
Jun1el <latorrevasquezandressebastian@gmail.com>
2025-09-21 13:01:00 -0500

    Refactor: Estructura de funcion palindrome

--- src/main/java/SpringPractice/controllers/PalindromoRest.java
+++ src/main/java/SpringPractice/controllers/PalindromoRest.java
@@ -8,17 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class PalindromoRest {
     @GetMapping("/palindrome/{palabra}")
     public String palindromo(@PathVariable String palabra) {
-        boolean esPalindromo = true;
-        for (int i = 0; i < palabra.length() / 2; i++) {
-            if (palabra.charAt(i) != palabra.charAt(palabra.length() - i - 1)) {
-                esPalindromo = false;
-                break;
-            }
-        }
-        if (esPalindromo) {
+        if (isPalindromo(palabra)) {
             return "La palabra " +palabra+ " Si es Palindromo";
         } else {
             return "La palabra "+palabra+" No es Palindromo";
         }
+    }
+    public boolean isPalindromo(String palabra) {
+        for (int i = 0; i < palabra.length() / 2; i++) {
+            if (palabra.charAt(i) != palabra.charAt(palabra.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
